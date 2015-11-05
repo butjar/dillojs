1c46dd0260fd64701b7a0c8a4d9230398bdf0460
butjar <martin.t.fleischer@gmail.com>
2015-11-05 09:34:45 +0100

    Add dillojs API
    Add:
    - server.js
    - npm package
    - upstart job

---
+++ api/server.js
@@ -0,0 +1,92 @@
+// Require scripts
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose   = require('mongoose'),
+    MongoClient = require('mongodb').MongoClient,
+    //path = require("path"),
+    cors = require('cors')
+
+// Base setup
+var app = express();
+var port = process.env.PORT || 8080;
+var router = express.Router();
+var Dillo = mongoose.model('Dillo', { weight: Number, alive: Boolean });
+
+//app.use(express.static(path.join(__dirname, '../', 'web/public')));
+app.use(cors());
+
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost:27017/dillojs');
+
+// CRUD
+router.route('/dillo')
+
+// CREATE
+  .post(function(req, res) {
+    console.log("Create Dillo");
+    var dillo = new Dillo({
+      weight: req.body.weight,
+      alive: req.body.alive
+    });
+
+    dillo.save(function(err) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(dillo);
+    });
+  })
+
+// READ
+  .get(function(req, res) {
+    console.log("Return all dillos");
+    Dillo.find(function(err, dillos) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(dillos);
+  });
+});
+
+router.route('/dillo/:dillo_id')
+
+  .get(function(req, res) {
+    Dillo.findById(req.params.dillo_id, function(err, dillo) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(dillo);
+    });
+  })
+
+//UPDATE
+  .put(function(req, res) {
+    Dillo.findById(req.params.dillo_id, function(err, dillo) {
+      if (err) {
+        res.send(err);
+      }
+      console.log(req.body);
+      dillo.weight = req.body.weight;
+      dillo.alive = req.body.alive;
+      dillo.save(function(err) {
+        if (err) {
+          res.send(err);
+        }
+        res.json(dillo);
+      });
+    });
+  })
+  
+// DELETE
+  .delete(function(req, res) {
+    Dillo.remove({ _id: req.params.dillo_id }, function(err, dillo) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
+
+// Start app
+app.use('/api', router);
+app.listen(port);
