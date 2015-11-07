46f3c62e62373c5168000401273674a1821dc6fa
butjar <martin.t.fleischer@gmail.com>
2015-11-07 11:12:12 +0100

    Remove rubocop offense in attributes

--- chef/attributes/default.rb
+++ chef/attributes/default.rb
@@ -3,7 +3,7 @@
 default['dillojs']['home'] = '/opt/dillojs'
 default['dillojs']['api']['home'] = "#{default['dillojs']['home']}/api"
 default['dillojs']['web']['home'] = "#{default['dillojs']['home']}/web"
-default['dillojs']['npm']['packages']['global'] = [ 'bower', 'brunch' ]
+default['dillojs']['npm']['packages']['global'] = %w(bower brunch)
 default['dillojs']['nginx']['root_dir'] = '/usr/share/nginx/www/'
 
 default['nodejs']['install_method'] = 'binary'
