a26914a876ad8d0790105c3313ca3574a25c46c0
butjar <martin.t.fleischer@gmail.com>
2015-11-09 10:01:18 +0100

    Add a test for nginx container
    - Right now unfortunately it's only possible to run tests against one
      container for a test run. Write a spec for the nginx container.
    - Exclude test case that checks the port because the container doesn't
      have netstat installed. Disable this test.
    - Add names for the containers to be sure to exec into the correct
      container.

---
+++ test/spec/docker/nginx_spec.rb
@@ -0,0 +1,12 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+describe 'dillojs' do
+  describe 'nginx' do
+    before(:all) do
+      set :docker_container, 'dillojs-nginx'
+    end
+      include_examples 'nginx'
+    end
+  end
